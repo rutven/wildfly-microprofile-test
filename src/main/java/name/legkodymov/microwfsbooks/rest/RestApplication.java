3a98c2d3b83dc5983ab1d3ad93e2ec3af0c577d8
Sergey Legkodymov <rutven@gmail.com>
2019-04-04 16:10:58 +0300

    Added modifications from thorntail demo

---
+++ src/main/java/name/legkodymov/microwfsbooks/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package name.legkodymov.microwfsbooks.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
